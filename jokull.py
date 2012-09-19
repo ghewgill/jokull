4ecc33730434a727398b80de4845012e296712b6
Greg Hewgill <greg@hewgill.com>
2012-09-19 08:43:58 +0000

    send command output to specified stream

--- jokull.py
+++ jokull.py
@@ -4,39 +4,39 @@ import sys
 
 import libjokull
 
-def do_create(session, args):
+def do_create(out, session, args):
     session.create_vault(args[2])
 
-def do_delete(session, args):
+def do_delete(out, session, args):
     if len(args) >= 4:
         session.delete_archive(args[2], args[3])
     else:
         session.delete_vault(args[2])
 
-def do_get(session, args):
+def do_get(out, session, args):
     f = session.get(args[2], args[3])
     with open(args[4], "wb") as outf:
         shutil.copyfileobj(f, outf)
 
-def do_jobs(session, args):
+def do_jobs(out, session, args):
     jobs = session.list_jobs(args[2])
-    pprint.pprint(jobs)
+    pprint.pprint(jobs, stream=out)
 
-def do_request(session, args):
+def do_request(out, session, args):
     if len(args) >= 4:
         r = session.new_job(args[2], archive_id=args[3])
     else:
         r = session.new_job(args[2])
-    print(r)
+    print(r, file=out)
 
-def do_upload(session, args):
+def do_upload(out, session, args):
     with open(args[3], "rb") as f:
         r = session.upload_archive(args[2], f)
-        print(r)
+        print(r, file=out)
 
-def do_vaults(session, args):
+def do_vaults(out, session, args):
     vaults = session.list_vaults()
-    pprint.pprint(vaults)
+    pprint.pprint(vaults, stream=out)
 
 Commands = {
     "create": do_create,
@@ -54,7 +54,7 @@ def main():
         print("Unknown command: {}".format(sys.argv[1]))
         sys.exit(1)
     session = libjokull.Jokull()
-    fn(session, sys.argv)
+    fn(sys.stdout, session, sys.argv)
 
 if __name__ == "__main__":
     main()
